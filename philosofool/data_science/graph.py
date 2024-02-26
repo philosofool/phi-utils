aca83092d29c4fe19a12aa023696dfc7a8ef3928
Stephen Lenhart <stephen.lenhart@lockerdome.com>
2024-02-25 18:27:55 -0700

    Add MetricGraph

--- philosofool/data_science/graph.py
+++ philosofool/data_science/graph.py
@@ -1,7 +1,14 @@
 """Functions for working with graphs"""
 
-from collections.abc import Hashable, Iterable, Mapping
+from collections.abc import Hashable, Iterable, Mapping, Callable
+from graphlib import TopologicalSorter
 from typing import Any
+
+import numpy as np  # noqa: F401
+import pandas as pd
+from numpy.typing import ArrayLike
+
+# from philosofool.functional.functional import compose_function
 
 
 def get_ancestors(value: Hashable, graph: Mapping[Any, Iterable], seen=None) -> set:
@@ -17,15 +24,68 @@ def get_ancestors(value: Hashable, graph: Mapping[Any, Iterable], seen=None) -> 
     return seen
 
 
-def test_get_ancestors():
-    """Test of get_ancestors."""
-    value = 'a'
-    graph = {'a': ('b', 'c', 'd'), 'c': ('b', 'e'), 'd': ('e', 'f')}
-    result = get_ancestors(value, graph)
-    expected = {'f', 'd', 'b', 'c', 'e'}
-    assert result == expected
-    print("test passed.")
+class MetricGraph:
+    """Calculate metrics from a model of metrics.
+
+    The model of metrics is a mapping of metrics to callables and metrics to dependencies.
+    The class handles the calculation of metrics in a Pandas dataframe.
+
+    Follows standard graphlib conventions: a graph is a mapping from Hashable elements to
+    a Hashable sequence.
+    """
+
+    def __init__(self, dependency_graph: Mapping[Any, tuple[Hashable, ...]], metric_functions: Mapping[Any, Callable]):
+        self.metric_functions = metric_functions
+        self.dependency_graph = dependency_graph
+
+    @classmethod
+    def from_model(cls, model: Mapping[Any, tuple[Callable, tuple[Hashable, ...]]]) -> 'MetricGraph':
+        """Construct an instance from a mapping of keys to the metric function and dependency names."""
+        metric_functions = {}
+        dependency_graph = {}
+        for key, (fn, deps) in model.items():
+            metric_functions[key] = fn
+            dependency_graph[key] = deps
+        return cls(dependency_graph, metric_functions)
+
+    def calculate_metric(self, metric: Hashable, calculated_metrics: Mapping[Any, ArrayLike]) -> ArrayLike:
+        """Calculate metric."""
+        calculator = self.metric_functions[metric]
+        dependencies = self.dependency_graph[metric]
+        data = [calculated_metrics[metric] for metric in dependencies]
+        return calculator(*data)
+
+    def calculate_metrics(self, df: pd.DataFrame, metrics: Iterable[Hashable]) -> Mapping[Any, ArrayLike]:
+        """Calculate the metrics from dataframe."""
+        sorted_metrics_and_dependencies = self._sort_metrics_topologically(
+            self.get_metric_dependencies(metrics).union(metrics)
+        )
+        calculated_metrics = {}
+        for metric in sorted_metrics_and_dependencies:
+            match df.get(metric):
+                case None:
+                    calculated_metrics[metric] = self.calculate_metric(metric, calculated_metrics)
+                case value:
+                    calculated_metrics[metric] = value
+        return {metric: calculated_metrics[metric] for metric in metrics}
+
+    def add_metrics(self, df: pd.DataFrame, metrics: Iterable[Hashable]) -> pd.DataFrame:
+        """Add the metrics to a dataframe."""
+        calculated_metrics = self.calculate_metrics(df, metrics)
+        return df.assign(calculated_metrics)  # type: ignore Pandas is not hinted for assign, but accepts dicts.
 
 
-if __name__ == '__main__':
-    test_get_ancestors()
+    def get_metric_dependencies(self, metrics: Iterable[Hashable]) -> set:
+        """Get the dependencies needed to calculate metrics."""
+        dependencies = set()
+        for metric in metrics:
+            metric_ancestors = get_ancestors(metric, self.dependency_graph, seen=dependencies)
+            dependencies = dependencies.union(metric_ancestors)
+        return dependencies
+
+    def _topologically_sorted_metrics(self) -> Mapping[Any, int]:
+        sorted_metrics = TopologicalSorter(self.dependency_graph).static_order()
+        return {metric: i for i, metric in enumerate(sorted_metrics)}
+
+    def _sort_metrics_topologically(self, metrics: Iterable[Hashable]) -> list[Hashable]:
+        return sorted(metrics, key=lambda metric: self._topologically_sorted_metrics()[metric])
